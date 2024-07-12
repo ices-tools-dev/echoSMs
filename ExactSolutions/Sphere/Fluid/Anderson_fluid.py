38df51e73594ffc4c12ba39e1b59450b96f41278
Gavin Macaulay <gavin@macaulay.co.nz>
2024-07-12 15:22:16 +1200

    No functionality changes: follow PEP8 & address pylint items

--- ExactSolutions/Sphere/Fluid/Anderson_fluid.py
+++ ExactSolutions/Sphere/Fluid/Anderson_fluid.py
@@ -1,17 +1,13 @@
-# -*- coding: utf-8 -*-
-'''
-Calculate backscatter by a fluid sphere using Anderson (1950)
-JASA, 22: 426-431
-https://doi.org/10.1121/1.1906621
+"""Calculate backscatter by a fluid sphere using Anderson (1950).
 
-jech
-'''
+JASA, 22: 426-431, https://doi.org/10.1121/1.1906621
+"""
 
+import sys
+from pathlib import Path
 import numpy as np
 from scipy.special import spherical_jn, spherical_yn
-import sys
 import matplotlib.pyplot as plt
-from pathlib import Path
 
 ###
 # physical parameters of the sphere and surrounding water
@@ -31,7 +27,7 @@ a = 0.01
 # density of the water (kg/m^3)
 rho_water = 1026.8
 # density of the sphere (kg/m^3)
-rho_sphere = 1028.9 
+rho_sphere = 1028.9
 
 # sound speed (h) and density (g) contrasts
 g = rho_sphere/rho_water
@@ -57,18 +53,18 @@ freq_step = 2000.0
 # the number of frequencies
 freq_n = int((freq_max-freq_min)/freq_step)+1
 # frequencies (Hz) at which to calculate scattering
-freq_Hz = np.arange(freq_min, freq_max+freq_step, freq_step) 
+freq_Hz = np.arange(freq_min, freq_max+freq_step, freq_step)
 
 ###
 # print the parameters and confirm calculations
 print('water density: {}, sound-speed water: {}'.format(rho_water, c_water))
 print('sphere density: {}, sound-speed sphere: {}'.format(rho_sphere, c_sphere))
-print('g: {}, h: {}'.format(round(g,3), round(h,3)))
+print('g: {}, h: {}'.format(round(g, 3), round(h, 3)))
 print('sphere radius: {}'.format(a))
-print('ka range: {} to {}'.format(round(2*np.pi*freq_min*a/c_water,3),
-                                  round(2*np.pi*freq_max*a/c_water,3)))
+print('ka range: {} to {}'.format(round(2*np.pi*freq_min*a/c_water, 3),
+                                  round(2*np.pi*freq_max*a/c_water, 3)))
 answ = input('Continue? [y/n]')
-if (answ == 'n'):
+if answ == 'n':
     sys.exit()
 
 ###
@@ -76,7 +72,7 @@ if (answ == 'n'):
 # reflectivity coefficient
 refl = []
 # ka
-ka = [] 
+ka = []
 
 # real component
 real = 0.0
@@ -92,18 +88,14 @@ for f in freq_Hz:
     ka_water = (2*np.pi*f/c_water)*a
     ka.append(ka_water)
     for m in range(order_max):
-        sphjkas = (m/ka_sphere)*spherical_jn(m, ka_sphere)-spherical_jn(m+1,
-                ka_sphere)
-        sphjkaw = (m/ka_water)*spherical_jn(m, ka_water)-spherical_jn(m+1,
-                ka_water)
-        sphykas = (m/ka_sphere)*spherical_yn(m, ka_sphere)-spherical_yn(m+1,
-                ka_sphere)
-        sphykaw = (m/ka_water)*spherical_yn(m, ka_water)-spherical_yn(m+1,
-                ka_water)
+        sphjkas = (m/ka_sphere)*spherical_jn(m, ka_sphere)-spherical_jn(m+1, ka_sphere)
+        sphjkaw = (m/ka_water)*spherical_jn(m, ka_water)-spherical_jn(m+1, ka_water)
+        sphykas = (m/ka_sphere)*spherical_yn(m, ka_sphere)-spherical_yn(m+1, ka_sphere)
+        sphykaw = (m/ka_water)*spherical_yn(m, ka_water)-spherical_yn(m+1, ka_water)
         alphaw = (2.*m+1.)*sphjkaw
         alphas = (2.*m+1.)*sphjkas
         beta = (2.*m+1)*sphykaw
-        numer = (alphas/alphaw)*(spherical_yn(m, ka_water)/spherical_jn(m, ka_sphere))-\
+        numer = (alphas/alphaw)*(spherical_yn(m, ka_water)/spherical_jn(m, ka_sphere)) - \
                 ((beta/alphaw)*(g*h))
         denom = (alphas/alphaw)*(spherical_jn(m, ka_water)/spherical_jn(m, ka_sphere))-(g*h)
         cscat = numer/denom
@@ -115,8 +107,8 @@ for f in freq_Hz:
     real = 0.0
 
 # convert to numpy arrays
-refl = np.array(refl, dtype = float)
-ka = np.array(ka, dtype = float)
+refl = np.array(refl, dtype=float)
+ka = np.array(ka, dtype=float)
 
 # convert to target strength (TS dB re m^2)
 # S is the cross-sectional area of the sphere
@@ -126,19 +118,17 @@ TS = 10*np.log10(refl**2*S)-10*np.log10(4*np.pi)
 
 ###
 # plot reflectivity
-#plt.plot(ka, refl)
-#plt.plot(freq_Hz/1000., refl)
+# plt.plot(ka, refl)
+# plt.plot(freq_Hz/1000., refl)
 # plot TS
 plt.plot(freq_Hz/1000., TS)
 plt.show(block=False)
 
 # write to a file
 outfile = Path('./Anderson_output.csv')
-ofl = open(str(outfile), 'w')
-hdr = 'f_kHz,R,TS\n'
-ofl.write(hdr)
-for i in range(freq_n):
-    ofl.write(','.join([str(round(freq_Hz[i]/1000, 1)), 
-                        str(round(refl[i], 9)), str(round(TS[i], 3))])+'\n')
-ofl.close()
-
+with open(str(outfile), 'w', encoding='utf-8') as ofl:
+    hdr = 'f_kHz,R,TS\n'
+    ofl.write(hdr)
+    for i in range(freq_n):
+        ofl.write(','.join([str(round(freq_Hz[i]/1000, 1)),
+                            str(round(refl[i], 9)), str(round(TS[i], 3))])+'\n')
