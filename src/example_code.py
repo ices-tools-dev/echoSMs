121be4cfcfe6bf4fa082c208ff9d7f70c97fc97a
Gavin Macaulay <gavin@macaulay.co.nz>
2024-08-08 16:13:57 +1200

    Move towards a documentation style for the examples

--- src/example_code.py
+++ src/example_code.py
@@ -17,64 +17,83 @@ for n in rm.model_names():
 bm = BenchMarkData()
 bmf = bm.dataset_freq()
 
-# %%
-####################################################################################################
+mss = MSSModel()
+
+# %% ###############################################################################################
 # Run the modal series solution model with different parameters.
 
-mss = MSSModel()
 print(f'The {mss.short_name} model supports boundary types of {mss.model_types}.')
 
-# Get the model parameters used in Jech et al. (2015) for a particular model.
-# m is a dictionary that contains model parameters
-s = rm.get_model_specification('weakly scattering sphere')
-m = rm.get_model_parameters('weakly scattering sphere')  # a subset of s
+# Run the mss model and compare to the benchmark values
 
-# Calculate a single TS using the MSS model and the weakly scattering sphere model parameters.
+# This is the mapping between model name from ReferenceModels and the appropriate column of data in
+# BenchMarkData.
+models = [('weakly scattering sphere', 'Sphere_WeaklyScattering'),
+          ('fixed rigid sphere', 'Sphere_Rigid'),
+          ('pressure release sphere', 'Sphere_PressureRelease')]
+
+for model in models:
+    # Get the model parameters used in Jech et al. (2015) for a particular model.
+    # m is a dictionary that contains model parameters
+    s = rm.get_model_specification(model[0])
+    m = rm.get_model_parameters(model[0])  # a subset of s
+
+    # Add frequencies and angle to the model parameters
+    m['f'] = bmf['Frequency_kHz']*1e3  # [Hz]
+    m['theta'] = 90.0
+
+    # and run these
+    ts = mss.calculate_ts(m, model_type=s['model_type'])
+
+    # Plot the mss model and benchmark results
+    fig, axs = plt.subplots(2, 1, sharex=True)
+    axs[0].plot(m['f']/1e3, ts, label='New code')
+    axs[0].plot(bmf['Frequency_kHz'], bmf[model[1]], label='Benchmark')
+    axs[0].set_ylabel('TS re 1 m$^2$ [dB]')
+    axs[0].legend()
+
+    # Plot difference between benchmark values and newly calculated mss model values
+    axs[1].plot(m['f']*1e-3, ts-bmf[model[1]])
+    axs[1].set_xlabel('Frequency [kHz]')
+    axs[1].set_ylabel(r'$\Delta$ TS [dB]')
+
+    plt.suptitle(model[0])
+
+# %% ###############################################################################################
+
+# How to calculate a single TS using the MSS model
+m = rm.get_model_parameters('weakly scattering sphere')
 ts = mss.calculate_ts_single(**m, theta=90.0, f=12000, model_type=s['model_type'])
 
-# Add some variation to the model parameters
-m['f'] = bmf['Frequency_kHz']*1e3  # [Hz]
-m['theta'] = 90.0
-# and run these parameters
-ts = mss.calculate_ts(m, model_type='fluid filled')  # does model_type override the one in m?
+# %% ###############################################################################################
 
-# Plot the mss model and benchmark results
-plt.plot(m['f']/1e3, ts, label='New code')
-plt.plot(bmf['Frequency_kHz'], bmf['Sphere_WeaklyScattering'], label='Benchmark')
-plt.xlabel('Frequency [kHz]')
-plt.ylabel('TS re 1 m$^2$ [dB]')
-plt.legend()
-plt.show()
-
-# Plot difference between benchmark values and newly calculated mss model values
-plt.plot(m['f']*1e-3, ts-bmf['Sphere_WeaklyScattering'])
-plt.xlabel('Frequency [kHz]')
-plt.ylabel(r'$\Delta$ TS [dB]')
-plt.title('New TS - benchmark TS')
-plt.show()
-
-# Add more variations to the model parameters
+# Can add more variations to the model parameters
+m = rm.get_model_parameters('weakly scattering sphere')
 m['f'] = np.linspace(12, 200, num=800) * 1e3  # [Hz]
 m['target_rho'] = np.arange(1020, 1030, 1)  # [kg/m^3]
 m['theta'] = [0, 90.0, 180.0]
-params_df = Utils.df_from_dict(m)
-# and run
-ts = mss.calculate_ts(params_df, model_type='fluid filled')
+# can convert this to a dataframe
+models_df = Utils.df_from_dict(m)
+# could also make a DataFrame of parameters that are not just the combination of all input
+# parameters. This offers a way to specify a more tailored set of model parameters.
 
-# Add the ts to the params dataframe for ease of selecting and plotting the results
-results = params_df
-results['ts'] = ts
+print(f'Running {len(models_df)} models')
+# and run
+ts = mss.calculate_ts(models_df, model_type='fluid filled')
+
+# And can then add the ts to the params dataframe for ease of selecting and plotting the results
+models_df['ts'] = ts
 
 # plot some of the results
 for rho in m['target_rho']:
-    r = results.query('target_rho == @rho and theta==90')
+    r = models_df.query('target_rho == @rho and theta==90')
     plt.plot(r['f']/1e3, r['ts'], label=f'{rho:.0f}')
 
 plt.xlabel('Freq [kHz]')
 plt.ylabel('TS re 1 m$^2$ [dB] ')
 plt.legend(title='Density [kg m$^{-3}$]')
 
-
+# %% ###############################################################################################
 # Example of model parameters not from the benchmarks
 params = {'medium_rho': [1000, 1250, 1500],
           'medium_c': np.arange(1400, 1600, 100),
@@ -86,11 +105,14 @@ params = {'medium_rho': [1000, 1250, 1500],
 
 # Instead of converting those to a dataframe, an xarray can be used.
 params_xa = Utils.xa_from_dict(params)
+
+# how many models runs would that be?
+print(f'Running {np.prod(params_xa.shape)} models!')
+
 # and is called the same way as for the dataframe
 ts = mss.calculate_ts(params_xa, model_type='fluid filled')
 
-# %%
-####################################################################################################
+# %% ###############################################################################################
 # Run the prolate spheroid modal series model.
 
 psms = PSMSModel()
