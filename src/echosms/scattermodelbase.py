f4b762ec6b20e7ac4b614ab80f86099ebd4988a5
Gavin Macaulay <gavin@macaulay.co.nz>
2024-08-11 15:58:36 +1200

    Add abstractmethods to define model class calling functions

--- src/echosms/scattermodelbase.py
+++ src/echosms/scattermodelbase.py
@@ -1,9 +1,10 @@
 """Base class for scatter model classes."""
 
+import abc
 import numpy as np
 
 
-class ScatterModelBaseClass:
+class ScatterModelBaseClass(abc.ABC):
     """Base class for a class that provides a scattering model.
 
     All scattering models should inherit from this class and have a name that
@@ -11,7 +12,7 @@ class ScatterModelBaseClass:
     """
 
     # pylint: disable=too-many-instance-attributes
-
+    @abc.abstractmethod
     def __init__(self):
         self.long_name = ''  # the name in words
         self.short_name = ''  # an abbreviation
@@ -20,3 +21,13 @@ class ScatterModelBaseClass:
         self.shapes = []  # the target shapes that this model can simulate
         self.max_frequency = np.nan  # [Hz]
         self.min_frequency = np.nan  # [Hz]
+
+    @abc.abstractmethod
+    def calculate_ts(self):
+        """Calculate the TS for many parameter sets."""
+        pass
+
+    @abc.abstractmethod
+    def calculate_ts_single(self):
+        """Calculate the TS for one parameter set."""
+        pass
