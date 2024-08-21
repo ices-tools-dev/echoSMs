4f4e53a9dffd350c9a957bc7412584a3a4993c23
Gavin Macaulay <gavin@macaulay.co.nz>
2024-08-22 10:01:52 +1200

    Use benchmark_model parameter to choose model instead of hardcoding in here

--- src/example_code.py
+++ src/example_code.py
@@ -4,7 +4,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from echosms import MSSModel, PSMSModel, DCMModel
+from echosms import MSSModel, PSMSModel, DCMModel, ESModel
 from echosms import BenchmarkData
 from echosms import ReferenceModels
 from echosms import as_dataframe, as_dataarray
@@ -24,27 +24,31 @@ bmt = bm.angle_dataset
 
 # This is the mapping between model name from ReferenceModels and the appropriate column of data in
 # BenchMarkData and which model to use.
-models = {'mss': [('weakly scattering sphere', 'Sphere_WeaklyScattering'),
-                  ('fixed rigid sphere', 'Sphere_Rigid'),
-                  ('pressure release sphere', 'Sphere_PressureRelease'),
-                  ('gas filled sphere', 'Sphere_Gas'),
-                  ('spherical fluid shell with pressure release interior',
-                   'ShellSphere_PressureRelease'),
-                  ('spherical fluid shell with gas interior', 'ShellSphere_Gas'),
-                  ('spherical fluid shell with weakly scattering interior',
-                   'ShellSphere_WeaklyScattering')],
-          'dcm': [('fixed rigid finite cylinder', 'Cylinder_Rigid'),
-                  ('pressure release finite cylinder', 'Cylinder_PressureRelease'),
-                  ('gas filled finite cylinder', 'Cylinder_Gas'),
-                  ('weakly scattering finite cylinder', 'Cylinder_WeaklyScattering')],
-          # 'psms': [('fixed rigid prolate spheroid', 'ProlateSpheroid_Rigid'),
-          #          ('pressure release prolate spheroid', 'ProlateSpheroid_PressureRelease'),
-          #          ('gas filled prolate spheroid', 'ProlateSpheroid_Gas'),
-          #          ('weakly scattering prolate spheroid', 'ProlateSpheroid_WeaklyScattering')]
-          }
+models = [('weakly scattering sphere', 'Sphere_WeaklyScattering'),
+          ('fixed rigid sphere', 'Sphere_Rigid'),
+          ('pressure release sphere', 'Sphere_PressureRelease'),
+          ('gas filled sphere', 'Sphere_Gas'),
+          ('spherical fluid shell with pressure release interior', 'ShellSphere_PressureRelease'),
+          ('spherical fluid shell with gas interior', 'ShellSphere_Gas'),
+          ('spherical fluid shell with weakly scattering interior',
+           'ShellSphere_WeaklyScattering'),
+          ('fixed rigid finite cylinder', 'Cylinder_Rigid'),
+          ('pressure release finite cylinder', 'Cylinder_PressureRelease'),
+          ('gas filled finite cylinder', 'Cylinder_Gas'),
+          ('weakly scattering finite cylinder', 'Cylinder_WeaklyScattering')]
+# Remove PSMS ones until the PSMS model works
+# ('fixed rigid prolate spheroid', 'ProlateSpheroid_Rigid'),
+# ('pressure release prolate spheroid', 'ProlateSpheroid_PressureRelease'),
+# ('gas filled prolate spheroid', 'ProlateSpheroid_Gas'),
+# ('weakly scattering prolate spheroid', 'ProlateSpheroid_WeaklyScattering')]
 
-for model, names in models.items():
-    match model:
+for name, bm_name in models:
+    # Get the model parameters used in Jech et al. (2015) for a particular model.
+    s = rm.specification(name)
+    m = rm.parameters(name)
+
+    # create the appropriate model instance
+    match s['benchmark_model']:
         case 'mss':
             mod = MSSModel()
         case 'psms':
@@ -54,49 +58,45 @@ for model, names in models.items():
         case _:
             pass
 
-    print(f'The {mod.short_name.upper()} ({mod.long_name}) model supports boundary '
-          f'types of {mod.boundary_types}.')
+    # Add frequencies and angle to the model parameters
+    m['f'] = bm.freq_dataset['Frequency_kHz']*1e3  # [Hz]
+    m['theta'] = 90.0
 
-    for name in names:
-        # Get the model parameters used in Jech et al. (2015) for a particular model.
-        s = rm.specification(name[0])
-        m = rm.parameters(name[0])
+    # and run these
+    ts = mod.calculate_ts(m)
 
-        # Add frequencies and angle to the model parameters
-        m['f'] = bm.freq_dataset['Frequency_kHz']*1e3  # [Hz]
-        m['theta'] = 90.0
+    jech_index = np.mean(np.abs(ts - bmf[bm_name]))
 
-        # and run these
-        ts = mod.calculate_ts(m)
+    # Plot the mss model and benchmark results
+    fig, axs = plt.subplots(2, 1, sharex=True)
+    axs[0].plot(m['f']/1e3, ts, label='echoSMs')
+    axs[0].plot(bmf['Frequency_kHz'], bmf[bm_name], label='Benchmark')
+    axs[0].set_ylabel('TS re 1 m$^2$ [dB]')
+    axs[0].legend(frameon=False, fontsize=6)
 
-        jech_index = np.mean(np.abs(ts - bmf[name[1]]))
-
-        # Plot the mss model and benchmark results
-        fig, axs = plt.subplots(2, 1, sharex=True)
-        axs[0].plot(m['f']/1e3, ts, label='echoSMs')
-        axs[0].plot(bmf['Frequency_kHz'], bmf[name[1]], label='Benchmark')
-        axs[0].set_ylabel('TS re 1 m$^2$ [dB]')
-        axs[0].legend(frameon=False, fontsize=6)
-
-        # Plot difference between benchmark values and newly calculated mss model values
-        axs[1].plot(m['f']*1e-3, ts-bmf[name[1]], color='black')
-        axs[1].set_xlabel('Frequency [kHz]')
-        axs[1].set_ylabel(r'$\Delta$ TS [dB]')
-        axs[1].annotate(f'{jech_index:.2f} dB', (0.05, 0.80), xycoords='axes fraction',
-                        backgroundcolor=[.8, .8, .8])
-        plt.suptitle(name[0])
-        plt.show()
+    # Plot difference between benchmark values and newly calculated mss model values
+    axs[1].plot(m['f']*1e-3, ts-bmf[bm_name], color='black')
+    axs[1].set_xlabel('Frequency [kHz]')
+    axs[1].set_ylabel(r'$\Delta$ TS [dB]')
+    axs[1].annotate(f'{jech_index:.2f} dB', (0.05, 0.80), xycoords='axes fraction',
+                    backgroundcolor=[.8, .8, .8])
+    plt.suptitle(name[0])
+    plt.show()
 
 # %% ###############################################################################################
 # Run the benchmark models and compare to the angle-varying benchmark results.
-models = {'dcm': [('fixed rigid finite cylinder', 'Cylinder_Rigid'),
-                  ('pressure release finite cylinder', 'Cylinder_PressureRelease'),
-                  ('gas filled finite cylinder', 'Cylinder_Gas'),
-                  ('weakly scattering finite cylinder', 'Cylinder_WeaklyScattering')]
-          }
+models = [('fixed rigid finite cylinder', 'Cylinder_Rigid'),
+          ('pressure release finite cylinder', 'Cylinder_PressureRelease'),
+          ('gas filled finite cylinder', 'Cylinder_Gas'),
+          ('weakly scattering finite cylinder', 'Cylinder_WeaklyScattering')]
 
-for model, names in models.items():
-    match model:
+for name, bm_name in models:
+    # Get the model parameters used in Jech et al. (2015) for a particular model.
+    s = rm.specification(name)
+    m = rm.parameters(name)
+
+    # create the appropriate model instance
+    match s['benchmark_model']:
         case 'mss':
             mod = MSSModel()
         case 'psms':
@@ -105,36 +105,34 @@ for model, names in models.items():
             mod = DCMModel()
         case _:
             pass
+    # Get the model parameters used in Jech et al. (2015) for a particular model.
+    s = rm.specification(name)
+    m = rm.parameters(name)
 
-    for name in names:
-        # Get the model parameters used in Jech et al. (2015) for a particular model.
-        s = rm.specification(name[0])
-        m = rm.parameters(name[0])
+    # Add frequencies and angle to the model parameters
+    m['f'] = 38000  # [Hz]
+    m['theta'] = bmt['Angle_deg']
 
-        # Add frequencies and angle to the model parameters
-        m['f'] = 38000  # [Hz]
-        m['theta'] = bmt['Angle_deg']
+    # and run these
+    ts = mod.calculate_ts(m)
 
-        # and run these
-        ts = mod.calculate_ts(m)
+    jech_index = np.mean(np.abs(ts - bmt[bm_name]))
 
-        jech_index = np.mean(np.abs(ts - bmt[name[1]]))
+    # Plot the mss model and benchmark results
+    fig, axs = plt.subplots(2, 1, sharex=True)
+    axs[0].plot(m['theta'], ts, label='echoSMs')
+    axs[0].plot(bmt['Angle_deg'], bmt[bm_name], label='Benchmark')
+    axs[0].set_ylabel('TS re 1 m$^2$ [dB]')
+    axs[0].legend(frameon=False, fontsize=6)
 
-        # Plot the mss model and benchmark results
-        fig, axs = plt.subplots(2, 1, sharex=True)
-        axs[0].plot(m['theta'], ts, label='echoSMs')
-        axs[0].plot(bmt['Angle_deg'], bmt[name[1]], label='Benchmark')
-        axs[0].set_ylabel('TS re 1 m$^2$ [dB]')
-        axs[0].legend(frameon=False, fontsize=6)
-
-        # Plot difference between benchmark values and newly calculated mss model values
-        axs[1].plot(m['theta'], ts-bmt[name[1]], color='black')
-        axs[1].set_xlabel('Angle (°)')
-        axs[1].set_ylabel(r'$\Delta$ TS [dB]')
-        axs[1].annotate(f'{jech_index:.2f} dB', (0.05, 0.80), xycoords='axes fraction',
-                        backgroundcolor=[.8, .8, .8])
-        plt.suptitle(name[0])
-        plt.show()
+    # Plot difference between benchmark values and newly calculated mss model values
+    axs[1].plot(m['theta'], ts-bmt[bm_name], color='black')
+    axs[1].set_xlabel('Angle (°)')
+    axs[1].set_ylabel(r'$\Delta$ TS [dB]')
+    axs[1].annotate(f'{jech_index:.2f} dB', (0.05, 0.80), xycoords='axes fraction',
+                    backgroundcolor=[.8, .8, .8])
+    plt.suptitle(name[0])
+    plt.show()
 
 # %% ###############################################################################################
 # Some other ways to run models.
@@ -191,3 +189,18 @@ if False:  # cause it takes a long time to run (as multiprocess is not enabled i
 
 # and it can be inserted into params_xa
 # TODO once the data is returned in an appropriate form
+
+# %% ###############################################################################################
+# Use the ES model on a calibration sphere
+p = rm.parameters('Cu60 calibration sphere')
+p['theta'] = 90
+p['f'] = np.arange(10, 400, 0.5) * 1e3  # [kHz]
+
+es = ESModel()
+ts = es.calculate_ts(p)
+
+plt.plot(p['f']*1e-3, ts)
+
+# Can readily modify the parameters for a different sphere
+p['a'] = 0.063
+ts = es.calculate_ts(p)
