3b8afeccd254b7788cb9b0a4efa614a9826f3e98
Gavin Macaulay <gavin@macaulay.co.nz>
2024-12-09 17:21:40 +1300

    Sort out dwba example data to fit echoSMs coordinate system convention

--- src/echosms/dwbautils.py
+++ src/echosms/dwbautils.py
@@ -3,6 +3,7 @@ import numpy as np
 from pathlib import Path
 import sys
 from dataclasses import dataclass
+from scipy.interpolate import splprep, splev
 if sys.version_info >= (3, 11):
     import tomllib
 else:
@@ -95,14 +96,14 @@ class DWBAorganism():
 
     Attributes
     ----------
-    x :
-        The _x_-axis coordinates [m].
-    y :
-        The _y_-axis coordinates [m].
-    z :
-        The _z_-axis coordinates [m].
+    rv_pos : iterable[np.ndarray]
+        An interable of vectors of the 3D positions of the centre of each disc that
+        defines the target shape. Each vector should have three values corresponding to
+        the _x_, _y_, and _z_ coordinates [m] of the disc centre, as per the echoSMs
+        [coordinate system](https://ices-tools-dev.github.io/echoSMs/
+        conventions/#coordinate-systems).
     a :
-        The radii [m].
+        The radii [m] of each disc.
     g :
         The density contrast between medium and organism (organism divied by medium).
     h :
@@ -111,16 +112,32 @@ class DWBAorganism():
         A link to the source of the data.
     note :
         Information about the data.
+    rv_tan :
+        An interable of unit vectors of the tangent to the body axis at
+        the points given by (_x_, _y_, _z_). Each vector has three values corresponding to
+        the _x_, _y_, and _z_ components of the tangent vector [m]. If not given, unit
+        vectors along the positive _x_-axis are used.
     """
 
-    x: np.ndarray
-    y: np.ndarray
-    z: np.ndarray
+    rv_pos: np.ndarray
     a: np.ndarray
     g: np.ndarray
     h: np.ndarray
     source: str = ''
     note: str = ''
+    rv_tan: np.ndarray = None
+
+    def plot(self):
+        """Do a simple plot of the DWBA model data."""
+        import matplotlib.pyplot as plt
+
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        ax.plot(self.rv_pos[:, 0], self.rv_pos[:, 1], self.rv_pos[:, 2], '.')
+        ax.quiver(self.rv_pos[:, 0], self.rv_pos[:, 1], self.rv_pos[:, 2],
+                  self.rv_tan[:, 0], self.rv_tan[:, 1], self.rv_tan[:, 2], length=0.005)
+        # ax.set_aspect('equal')
+        ax.set(xlabel='x', ylabel='y', zlabel='z')
+        plt.show()
 
 
 class DWBAdata():
@@ -138,9 +155,27 @@ class DWBAdata():
         # Put the shapes into a dict of SDWBAorganism().
         self.dwba_models = {}
         for s in shapes['shape']:
-            organism = DWBAorganism(np.array(s['x']), np.array(s['y']), np.array(s['z']),
-                                    np.array(s['a']), np.array(s['g']), np.array(s['h']),
-                                    s.get('source', ''), s.get('note', ''))
+            # Some datasets have the x data listed from largest to smallest, but things work
+            # better with the reverse (particularily calculating rv_tan), so sort that.
+            if s['x'][0] > s['x'][-1]:
+                s['x'].reverse()
+                s['y'].reverse()
+                s['z'].reverse()
+                s['a'].reverse()
+                s['g'].reverse()
+                s['h'].reverse()
+
+            rv_pos = np.vstack((np.array(s['x']), np.array(s['y']), np.array(s['z']))).T
+
+            # Estimate rv_tan from a spline through rv_pos.
+            tck, u = splprep([s['x'], s['y'], s['z']])
+            rv_tan = np.vstack(splev(u, tck, der=1))
+            # Make sure the rv_tan are unit vectors
+            n = np.linalg.norm(np.vstack(rv_tan), axis=0)
+            rv_tan = (rv_tan / n).T
+
+            organism = DWBAorganism(rv_pos, np.array(s['a']), np.array(s['g']), np.array(s['h']),
+                                    s.get('source', ''), s.get('note', ''), rv_tan)
             self.dwba_models[s['name']] = organism
 
     def names(self):
