5a5b11846aadebaa224d2f8898741d72197ec7b1
Gavin Macaulay <gavin@macaulay.co.nz>
2025-08-20 13:31:03 +1200

    Add getting of openAPI.json back into mkdocs

--- docs/src/get_datastore_api_json.py
+++ docs/src/get_datastore_api_json.py
@@ -17,11 +17,13 @@ def on_pre_build(config):
     # This URL requires that FASTapi be available on the local machine when the docs are built
     url = 'http://127.0.0.1:8000/openapi.json'
 
-    r = requests.get(url)
+    try:
+        r = requests.get(url)
 
-    # If the requests.get call fails, this will silently fail and fall over to the existing
-    # .json file in the docs_dir.
-
-    if r.status_code == 200:
-        with open(Path(config["docs_dir"])/'datastore_api_openapi.json', 'w') as file:
-            json.dump(r.json(), file, indent=2)
+        if r.status_code == 200:
+            with open(Path(config["docs_dir"])/'datastore_api_openapi.json', 'w') as file:
+                json.dump(r.json(), file, indent=2)
+        else:
+            print('Failed to get openAPI.json file, using existing.')
+    except requests.exceptions.ConnectionError:
+        print('Failed to connect to server. openAPI.json not updated.')
