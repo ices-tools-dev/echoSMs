11e904af14968e61224c08dc4a71d51516273246
Gavin Macaulay <gavin@macaulay.co.nz>
2025-10-07 13:29:26 +1300

    Add V2 datastore API examples, update API docs too

--- data_store/src/api_examples.py
+++ data_store/src/api_examples.py
@@ -5,6 +5,7 @@
 
 import requests
 import pandas as pd
+import numpy as np
 import copy
 import seaborn as sns
 from PIL import Image
@@ -17,10 +18,13 @@ import matplotlib.pyplot as plt
 baseURI = 'http://127.0.0.1:8000/'
 baseURI = 'https://echosms-data-store-app-ogogm.ondigitalocean.app/'
 
+# %%
+# Examples using the v1 version of the API
 ds = []
 
 print('Getting list of datasets')
 d = requests.get(baseURI + 'v1/datasets')
+
 if d.status_code == 200:
     print(f'Received {len(d.json())} datasets')
 
@@ -41,17 +45,18 @@ if d.status_code == 200:
                     if a in s:
                         row[a] = s[a]
                     elif a == 'weight':  # default value for weight not present
-                        row[a] = 0.0
+                        row[a] = np.nan
                 ds.append(copy.deepcopy(row))
 
     # Put data datasets into a DataFrame for easier viewing
     df = pd.DataFrame(data=ds).set_index(['dataset_id', 'specimen_id'])
     print(df[['vernacular_name', 'model_type', 'shape_types']])
 
-    # A plot of specimen length/weight, coloured by species. Quite a few of the current
-    # specimens don't have a weight, so they show as zero in the plot.
-    obj = sns.scatterplot(data=df, x='length', y='weight',
+    # A plot of specimen length/weight, coloured by species.
+    # Filter out data where weight is NaN (i.e., not present)
+    obj = sns.scatterplot(data=df[df['weight'].notnull()], x='length', y='weight',
                           hue='vernacular_name', style='imaging_method')
+    obj.set_title('Specimens with length and weight values')
     # Move legend to outside of the axes
     sns.move_legend(obj, 'upper right', bbox_to_anchor=(1.6, 1))
     plt.show()
@@ -65,3 +70,65 @@ if d.status_code == 200:
         plt.imshow(Image.open(BytesIO(r.content)))
         plt.axis('off')
         plt.show()
+
+# %%
+# Examples using the v2 version of the API
+
+print('Getting specimen metadata')
+d = requests.get(baseURI + 'v2/specimens')
+
+if d.status_code == 200:
+    df = pd.DataFrame(data=d.json())
+    print(df[['vernacular_name', 'model_type']])
+
+    # A plot of specimen length/weight, coloured by species.
+    # Filter out data where weight is NaN (i.e., not present)
+    obj = sns.scatterplot(data=df[df['weight'].notnull()],
+                          x='length', y='weight',
+                          hue='vernacular_name', style='imaging_method')
+    obj.set_title('Specimens with length and weight values')
+    # Move legend to outside of the axes
+    sns.move_legend(obj, 'upper right', bbox_to_anchor=(1.6, 1))
+    plt.show()
+
+    # Get an image of one of the shapes and show it locally (or put the URL
+    # into a web browser manually)
+    print('Getting a specimen shape image...')
+    r = requests.get(baseURI + 'v2/specimen/' + 'CLAY_HORNE_B' + '/image')
+
+    if r.status_code == 200:
+        plt.imshow(Image.open(BytesIO(r.content)))
+        plt.axis('off')
+        plt.show()
+
+
+# %%
+# An example showing how to get all specimens metadata and shapes from the datastore
+# and store (and access) them locally.
+
+d = requests.get(baseURI + 'v2/specimens')
+
+if d.status_code == 200:
+    ds = []
+    for s in d.json():  # iterate over all specimens in the datastore
+        ds.append(s)
+
+        print(f'Getting shape(s) for specimen {s["id"]}')
+        shape = requests.get(baseURI + '/v2/specimen/' + s['id'] + '/shape')
+        ds[-1]['shapes'] = shape.json()
+
+    # Put into a Pandas dataframe
+    df = pd.DataFrame(data=ds)
+
+    # Save the dataframe to a compressed file for later use
+    df.to_pickle('echosms_anatomical_datastore.gz')
+
+    # That file can be read back and used without
+    # online access to the echoSMs datastore API
+    dff = pd.read_pickle('echosms_anatomical_datastore.gz')
+
+    # An example of querying the dataframe
+    print(dff.query('genus == "Gadus"')[['id', 'vernacular_name', 'shape_type', 'imaging_method']])
+
+    # The shape data is in the 'shape' column, for example:
+    print(dff.iloc[0]['shapes'])
--- docs/datastoreAPI.md
+++ docs/datastoreAPI.md
@@ -10,11 +10,9 @@ The anatomical data store API is implemented as a RESTful web API with calls to:
 1. Obtain specimen information and model definitions and parameters.
 1. Access the full dataset associated with a model (this includes the raw data, processing scripts, intermediate data, etc).
 
-API endpoints to modify the data store have not yet been created as the data store can be manually loaded given the relatively low rate of expected model uploading.
+A testing server is available with some sample data, along with the API [documentation](https://echosms-data-store-app-ogogm.ondigitalocean.app/docs). Example Python code that demonstrates use of the API is available [here](https://github.com/ices-tools-dev/echoSMs/blob/main/data_store/src/api_examples.py). That code also includes an example of how to download all specimen data to a local file for offline access.
 
-A testing server has been setup with some sample data. Live API documentation is available from the test [server](https://echosms-data-store-app-ogogm.ondigitalocean.app/docs).
-
-Two versions of the API are provided. V1 provides data in a dataset structure, where one dataset contains multiple specimens. V2 provides a flattened view of this, where there are only specimens. It is not yet clear which way is preferred so, for testing, both are provided.
+Two versions of the API are provided. V1 provides data in a dataset structure, where one dataset contains multiple specimens. V2 provides a flattened view of this, where there are only specimens. It is not yet clear which way is preferred so, for testing, both are provided. API endpoints to modify the data store have not yet been created as the data store can be manually loaded given the relatively low rate of expected model uploading.
 
 Some example API calls are:
 
@@ -33,4 +31,3 @@ Some example API calls are:
     - A list of all specimens with genus of [Champsocephalus](https://echosms-data-store-app-ogogm.ondigitalocean.app/v2/specimens?genus=Champsocephalus)
     - Shape data from the [CLAY_HORNE_A](https://echosms-data-store-app-ogogm.ondigitalocean.app/v2/specimen/CLAY_HORNE_B/shape) specimen and an [image](https://echosms-data-store-app-ogogm.ondigitalocean.app/v2/specimen/CLAY_HORNE_B/image) of that specimen
 
-An example Python program that uses the V1 API is available [here](https://github.com/ices-tools-dev/echoSMs/blob/main/data_store/src/api_examples.py).
