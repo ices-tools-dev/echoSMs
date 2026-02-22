5f527ad2599d24a432179a198dc7c62deb33fdd7
Gavin Macaulay <gavin@macaulay.co.nz>
2026-02-23 10:27:16 +1300

    update api examples for revised schema

--- data_store/src/api_examples.py
+++ data_store/src/api_examples.py
@@ -25,27 +25,17 @@ d = requests.get(baseURI + 'v2/specimens')
 
 if d.status_code == 200:
     df = pd.DataFrame(data=d.json())
-    print(df[['vernacular_name', 'model_type']])
+    print(df[['vernacular_names', 'model_type']])
 
     # A plot of specimen length/weight, coloured by species.
     # Filter out data where weight is NaN (i.e., not present)
     obj = sns.scatterplot(data=df[df['weight'].notnull()],
                           x='length', y='weight',
-                          hue='vernacular_name', style='imaging_method')
+                          hue='species', style='imaging_method')
     obj.set_title('Specimens with length and weight values')
     # Move legend to outside of the axes
     sns.move_legend(obj, 'upper right', bbox_to_anchor=(1.6, 1))
     plt.show()
-
-    # Get an image of one of the shapes and show it locally (or put the URL
-    # into a web browser manually)
-    print('Getting a specimen shape image...')
-    r = requests.get(baseURI + 'v2/specimen/' + 'CLAY_HORNE_B' + '/image')
-
-    if r.status_code == 200:
-        plt.imshow(Image.open(BytesIO(r.content)))
-        plt.axis('off')
-        plt.show()
 
 
 # %%
@@ -58,8 +48,8 @@ d = requests.get(baseURI + 'v2/specimens?shape_type=outline')
 if d.status_code == 200:
     ds = []
     for s in d.json():  # iterate over all specimens with in the datastore
-        print(f'Getting all data for specimen {s["id"]}')
-        specimen = requests.get(baseURI + '/v2/specimen/' + s['id'] + '/data')
+        print(f'Getting all data for specimen {s["specimen_name"]}')
+        specimen = requests.get(baseURI + '/v2/specimen/' + s['uuid'] + '/data')
         ds.append(specimen.json())
 
     # Put into a Pandas dataframe
@@ -73,7 +63,7 @@ if d.status_code == 200:
     dff = pd.read_pickle('downloaded_anatomical_datastore.gz')
 
     # An example of querying the dataframe
-    print(dff.query('genus == "Gadus"')[['id', 'vernacular_name', 'shape_type', 'imaging_method']])
+    print(dff.query('genus == "Gadus"')[['vernacular_names', 'shape_type', 'imaging_method']])
 
     # The shape data is in the 'shape' column, for example:
     pprint.pprint(dff.iloc[0]['shapes'])
