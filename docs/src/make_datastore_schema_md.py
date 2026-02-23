c5e0f51a7b31d761fad70afc720892eec282e6f8
Gavin Macaulay <gavin@macaulay.co.nz>
2026-02-24 11:10:54 +1300

    Revert "Move datastore schema generation from mkdocs on_files hook to on_pre_build hook"
    This reverts commit bea5c4fbbbdc09a0b6a0ce7ac533c7b66cdceb4c.

--- docs/src/make_datastore_schema_md.py
+++ docs/src/make_datastore_schema_md.py
@@ -1,6 +1,7 @@
 """Make a Material for MkDocs markdown version of the anatomical datastore JSON schema."""
 
 from pathlib import Path
+from mkdocs.structure.files import File
 
 # The package that was used, but it generates poor-looking output
 # from json_schema_for_humans.generate import generate_from_filename
@@ -10,15 +11,17 @@ from pathlib import Path
 # structures used by echoSMs
 from schema_to_markdown import generate
 
-def on_pre_build(config):
+def on_files(files, config):
     """Create the schema markdown file.
 
     This function is called when mkdocs is run and converts the echoSMs anatomical
-    data store schema json file into a markdown file and places that in with the
-    rest of the markdown files.
+    data store schema json file into a markdown file, then adds that to the echoSMs
+    documentation that mkdocs generates.
     """
     schema_file = Path('data_store')/'schema'/'v1'/'anatomical_data_store.json'
-    schema_md_file = Path('docs')/'data_store_schema.md'
+    schema_md_dir = Path(config["site_dir"])/'schema'
+    schema_md_dir.mkdir(exist_ok=True, parents=True)
+    schema_md_file = schema_md_dir/'data_store_schema.md'
 
     # make doc from the JSON schema using json schema for humans
     # cc = gc(copy_css=True, expand_buttons=True, show_breadcrumbs=False,
@@ -31,3 +34,18 @@ def on_pre_build(config):
 
     # make doc from the JSON schema using echoSMs own code
     generate(schema_file, schema_md_file)
+
+    # Add to Files object
+    mkdfile = File(path=f'schema/{schema_md_file.name}',
+                   src_dir=config["site_dir"],
+                   dest_dir=config["site_dir"],
+                   use_directory_urls=config["use_directory_urls"])
+
+    files.append(mkdfile)
+
+    # Add the schema page in the 'Anatomical data store' section
+    for s in config['nav']:
+        if 'Anatomical data store' in s:
+            s['Anatomical data store'].insert(2, ({'Schema': mkdfile.src_path}))
+
+    return files
