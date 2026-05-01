d6a7e32a9bcdaaa4e35ec0543f0eed481340f628
Gavin Macaulay <gavin@macaulay.co.nz>
2026-05-01 15:26:18 +1200

    move back to using json-schema-for-humans for showing datastore schema

--- docs/datastore_schema.md
+++ docs/datastore_schema.md
@@ -1,11 +1,15 @@
 # Schema
 
-The dataset contents are specified by a [JSON schema](https://json-schema.org/) file stored in the echoSMs. The schema documents the required attributes, their structure, valid values, etc. The schema is a very technical document and is perhaps more easily understood via the interative online version.
+The dataset contents are specified by a [JSON schema](https://json-schema.org/) file stored in the echoSMs repository. The schema specifies the required attributes, their structure, valid values, etc.
 
-- [Browse](https://json-schema.app/view/%23?url=https%3A%2F%2Fraw.githubusercontent.com%2Fices-tools-dev%2FechoSMs%2Frefs%2Fheads%2Fmain%2Fdata_store%2Fschema%2Fv1%2Fanatomical_data_store.json) the schema online.
 - [View](https://github.com/ices-tools-dev/echoSMs/blob/main/data_store/schema/v1/anatomical_data_store.json) the raw JSON schema file. This is the authorative source of the schema.
+- [Browse](#schema-browser) an interactive version of the schema. This version may not show all the details - look at the raw schema file for full details.
 - [View](https://github.com/ices-tools-dev/echoSMs/tree/main/data_store/resources) an example datastore file.
 
 Datastore dataset files can be validated against the schema using online validators (e.g., [here](https://www.jsonschemavalidator.net/), [here](https://jsonschema.dev/), or [here](https://www.liquid-technologies.com/online-json-schema-validator)), or within your own code using a JSON schema validation library (e.g., [jsonschema-rs](https://github.com/Stranger6667/jsonschema/tree/master/crates/jsonschema-py) for Python and [jsonvalidate](https://cran.r-project.org/web/packages/jsonvalidate/vignettes/jsonvalidate.html) for R).
 
 It is not necessary to validate your data before submitting it to the datastore, but it will help the uploading happen faster (a validation is done during the uploading process and any dataset format problems will be identified then).
+
+## Schema browser
+
+<iframe src="../schema_doc.html" width=100% height=1000px style="border-width: 0"></iframe>
--- docs/src/make_datastore_schema_md.py
+++ docs/src/make_datastore_schema_md.py
@@ -4,48 +4,36 @@ from pathlib import Path
 from mkdocs.structure.files import File
 
 # The package that was used, but it generates poor-looking output
-# from json_schema_for_humans.generate import generate_from_filename
-# from json_schema_for_humans.generation_configuration import GenerationConfiguration as gc
-
-# Hacky version now used, written solely to do the echoSMs schema, so only supports schema
-# structures used by echoSMs
-from schema_to_markdown import generate
+from json_schema_for_humans.generate import generate_from_filename
+from json_schema_for_humans.generation_configuration import GenerationConfiguration as gc
 
 def on_files(files, config):
     """Create the schema markdown file.
 
     This function is called when mkdocs is run and converts the echoSMs anatomical
-    data store schema json file into a markdown file, then adds that to the echoSMs
-    documentation that mkdocs generates.
+    data store schema json file into an html file that is referenced into an
+    existing markdown file.
     """
     schema_file = Path('data_store')/'schema'/'v1'/'anatomical_data_store.json'
-    schema_md_dir = Path(config["site_dir"])/'schema'
-    schema_md_dir.mkdir(exist_ok=True, parents=True)
-    schema_md_file = schema_md_dir/'data_store_schema.md'
+    formatted_schema_file = Path(config["site_dir"])/'schema_doc.html'
 
     # make doc from the JSON schema using json schema for humans
-    # cc = gc(copy_css=True, expand_buttons=True, show_breadcrumbs=False,
-    #         link_to_reused_ref=False, show_toc=False, template_name='md',
-    #         template_md_options={'badge_as_image': True,
-    #                              'show_heading_numbers': False,
-    #                              'properties_table_columns':
-    #                              ['Property', 'Type', 'Title/Description']})
-    # generate_from_filename(schema_file, schema_md_file, config=cc)
+    cc = gc(expand_buttons=True,
+            link_to_reused_ref=False,
+            collapse_long_descriptions=True,
+            description_is_markdown=True,
+            with_footer=False,
+            template_name='js',
+            )
 
-    # make doc from the JSON schema using echoSMs own code
-    generate(schema_file, schema_md_file)
+    generate_from_filename(schema_file, formatted_schema_file, config=cc)
 
     # Add to Files object
-    mkdfile = File(path=f'schema/{schema_md_file.name}',
+    mkdfile = File(path=formatted_schema_file.name,
                    src_dir=config["site_dir"],
-                   dest_dir=config["site_dir"],
+                   dest_dir=f'{config["site_dir"]}',
                    use_directory_urls=config["use_directory_urls"])
 
     files.append(mkdfile)
-
-    # Add the schema page in the 'Anatomical data store' section
-    for s in config['nav']:
-        if 'Anatomical data store' in s:
-            s['Anatomical data store'].insert(2, ({'Schema': mkdfile.src_path}))
 
     return files
