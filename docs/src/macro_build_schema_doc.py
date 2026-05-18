44d14243fbd461ced3d9d65a8b40d85fd51205ae
Gavin Macaulay <gavin@macaulay.co.nz>
2026-05-18 14:32:41 +1200

    Prep work for moving to zensical: generate schema with a macro

--- docs/src/macro_build_schema_doc.py
+++ docs/src/macro_build_schema_doc.py
@@ -1,13 +1,25 @@
+from pathlib import Path
+from json_schema_for_humans.generate import generate_from_filename
+from json_schema_for_humans.generation_configuration import GenerationConfiguration as gc
+
 def define_env(env):
-    # Variables are accessible as {{ version }} in templates
-    env.variables["version"] = "1.0"
-
-    # Macros are called as {{ greet("World") }}
+    # Call this in a markdown document via:
+    # {{ datastore_schema_as_html() }}
     @env.macro
-    def schema_doc(name):
-        return 'ABCDEFG'
+    def datastore_schema_as_html():
+        schema_file = Path('data_store')/'schema'/'v1'/'anatomical_data_store.json'
+        html_file = Path('site')/'schema'/'schema_doc.html'
+        html_file.parent.mkdir(parents=True, exist_ok=True)
 
-    # Filters are applied as {{ "hello" | shout }}
-    @env.filter
-    def shout(text):
-        return text.upper()
+        # make doc from the JSON schema using json schema for humans
+        cc = gc(expand_buttons=True,
+                link_to_reused_ref=False,
+                collapse_long_descriptions=True,
+                description_is_markdown=True,
+                with_footer=False,
+                template_name='js',
+                )
+
+        generate_from_filename(schema_file, html_file, config=cc)
+
+        return '<iframe src="./schema/schema_doc.html" width=100% height=1000px style="border-width: 0"></iframe>'
